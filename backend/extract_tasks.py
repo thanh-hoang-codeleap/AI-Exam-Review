af728887456a50dfff7db48ae018f389cb5dfc71
Thanh Hoang <thanh.hoang@codeleap.de>
2025-05-08 16:34:34 +0700

    update folder location

--- backend/extract_tasks.py
+++ backend/extract_tasks.py
@@ -83,7 +83,7 @@ def extract_tasks(file_path):
     )
 
     file_name = os.path.basename(file_path)[:-4].replace(" ", "_")
-    output_file = f'backend/extracted_texts/{file_name}.txt'
+    output_file = f'extracted_texts/{file_name}.txt'
     if os.path.exists(output_file):
         os.remove(output_file)
 
